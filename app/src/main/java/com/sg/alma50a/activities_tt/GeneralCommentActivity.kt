5ff00dd36304b7dbc04f85677c5b6dc52aaa8a70
shlomo <shlomo.gevim@gmail.com>
2022-07-08 20:10:43 +0300

    8/7/2022  bffor adding GeneralCommentActivity

---
+++ app/src/main/java/com/sg/alma50a/activities_tt/GeneralCommentActivity.kt
@@ -0,0 +1,13 @@
+package com.sg.alma50a.activities_tt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sg.alma50a.R
+
+class GeneralCommentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_general_comment)
+    }
+}
